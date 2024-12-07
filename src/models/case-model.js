d59459ec346b7e372e3a9f34621228b255fdf66d
Diego Alejandro Muñoz Artunduaga <diego.ch.84@hotmail.com>
2024-12-07 14:21:26 -0500

    Agregación del modelo para casa

---
+++ src/models/case-model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose
+
+const caseSchema = new Schema({
+    caseAlarmName: {
+        type: String,
+        required: [true, 'Campo requerido'],
+        minLength:[5, 'Se requiere mínimo 5 caracteres'],
+        maxLength:[100, 'Se acepta máximo 100 caracteres']
+    },
+    caseClient: {
+        type: String,
+        required: [true, 'Campo requerido'],
+        minLength:[4, 'Se requiere mínimo 4 caracteres'],
+        maxLength:[80, 'Se acepta máximo 80 caracteres']
+    },
+    caseTeamInCharge:{
+        type: String,
+        required: [true, 'Campo requerido'],
+        minLength:[3, 'Se requiere mínimo 3 caracteres'],
+        maxLength:[100, 'Se acepta máximo 100 caracteres']
+    },
+    caseKeyWords:{
+        type: [String],
+        required: [true, 'Campo requerido'],
+        validate:
+        [
+            {
+            characterValidator: function (keyWords){
+                return keyWords.every(word => word.length >= 3 && word.length <= 50);
+                },
+                message: 'Cada palabra debe tener entre 3 y 50 caracteres'
+            },
+            {
+            inNullValidator: function (keyWords){
+                return keyWords.length > 0;
+                },
+                message: 'Los campos del arreglo no pueden ir vacios'
+            }
+        ]
+    },
+    userCreateDate: {type:Date, default:Date.now},
+    isActive: {type:Boolean, default:true}
+});
+
+module.exports = mongoose.model('case', caseSchema);
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -4,13 +4,12 @@ const userCtr = require('../controllers/user-controllers.js');
 const usersModel = require('../models/users-model.js');
 const auth = require('../helper/auth.js');
 
-// Rutas
+// Rutas del user
 router.post('/createUser', userCtr.createUser);
 router.get('/listUser', auth.TokenVerify, userCtr.listUsers);
 router.get('/listUsersById/:id', auth.TokenVerify, userCtr.listUsersById);
 router.put('/updateUserById/:id', auth.TokenVerify, userCtr.updateUserById);
 router.delete('/deleteUser/:id', auth.TokenVerify, userCtr.deleteUser);
 router.post('/login', userCtr.login);
-
 
 module.exports = router
