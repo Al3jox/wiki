c76df54cb34a3b104a00fbb4d062a7060ee38886
Diego Alejandro Muñoz Artunduaga <diego.ch.84@hotmail.com>
2025-02-08 14:52:33 -0500

    Ajuste de números de peticiones del ratelimit

--- src/helper/rateLimiter.js
+++ src/helper/rateLimiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-    windowMs: 5 * 60 * 1000, // 5 Minutos
-    limit: 2, // Número de peticiones por los minutos definidos
+    windowMs: 5 * 60 * 1000, // 5 Minutos, 60 segundos, 1000 milisegundos
+    limit: 20, // Número de peticiones por los minutos definidos
     standarHeaders: 'draft-8',
     legacyHeaders:false,
     message: 'Demasiadas solicitudes. Intenta de nuevo más tarde.',
